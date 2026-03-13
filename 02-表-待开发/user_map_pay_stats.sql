e6c1b71cc13de33158dec06aa3a13ee8b74a858a
Chenhao Wang <chwang1991@163.com>
2026-03-13 15:36:28 +0800

    1

--- 02-表-待开发/user_map_pay_stats.sql
+++ 02-表-待开发/user_map_pay_stats.sql
@@ -24,16 +24,13 @@ hive.mnv_ads_account_cn.acc_oplog_minicoin_details_day
 hive.mnv_ads_ugc_cn.map_consumption_details_day
 
 # 结果字段（字段名-类型-comment）
-
-map_id              varchar 地图ID
-ctype               int     地图类型（1-存档，2-地图）
-minicoin            bigint  迷你币消费数量
-minicoin_payer      bigint  迷你币消费人数
-minibean            bigint  迷你豆消费数量
-minibean_payer      bigint  迷你豆消费人数
-minipoint           bigint  迷你点消费数量
-minipoint_payer     bigint  迷你点消费人数
-dt                  varchar 数据起始日期：2024-01-01
+uin         bigint  用户ID
+map_id      varchar 地图ID
+ctype       int     地图类型（1-存档，2-地图）
+minicoin    bigint  迷你币消费数量
+minibean    bigint  迷你豆消费数量
+minipoint   bigint  迷你点消费数量
+dt          varchar 数据起始日期：2024-01-01
 
 */
 
@@ -45,56 +42,61 @@ args as (select '2026-01-01' as dt)
     where dt=(select dt from args)
 )
 ,minicoin_data as (
-    select coalesce(
+    select uin
+    ,coalesce(
          json_value(json_query(args,'lax $[1].ext' OMIT QUOTES),'lax $.map_id')
         ,json_value(json_query(args,'lax $[1].ext' OMIT QUOTES),'lax $.mapid')
         ,json_extract_scalar(args,'$[1].map_id')
     ) as map_id
     ,try_cast(sum(abs(diff)) as bigint) as minicoin
-    ,count(distinct uin) as minicoin_payer
     from hive.mnv_ads_account_cn.acc_oplog_minicoin_details_day
     where dt=(select dt from args)
     and diff<0
     and (why='gm.exec' and reason in (
-        'ministudio_shop'
+         'ministudio_shop'
         ,'kfz_iap_finish'
         ,'kfz_skin_buy'
         ,'kfz_iap_reward'
         ,'kfz_sub_coin_exchange'
         ,'kfz_unlock_pay_map'
     ))
-    group by 1
+    group by 1,2
 )
 ,minibean_data as (
-    select map_id
+    select try_cast(uin as bigint) as uin,map_id
     ,sum(pay_cnt) as minibean
-    ,count(distinct uin) as minibean_payer
     from hive.mnv_ads_ugc_cn.map_consumption_details_day
     where dt=(select dt from args)
     and event_code='bean_pay'
-    group by 1
+    group by 1,2
 )
 ,minipoint_data as (
-    select map_id
+    select try_cast(uin as bigint) as uin,map_id
     ,sum(pay_cnt) as minipoint
-    ,count(distinct uin) as minipoint_payer
     from hive.mnv_ads_ugc_cn.map_consumption_details_day
     where dt=(select dt from args)
     and event_code='point_pay'
-    group by 1
+    group by 1,2
 )
-select t1.map_id,t1.ctype
-,coalesce(minicoin,0)           as minicoin
-,coalesce(minicoin_payer,0)     as minicoin_payer
-,coalesce(minibean,0)           as minibean
-,coalesce(minibean_payer,0)     as minibean_payer
-,coalesce(minipoint,0)          as minipoint
-,coalesce(minipoint_payer,0)    as minipoint_payer
+,pay_data as (
+    select uin,map_id
+    ,sum(minicoin)  as minicoin
+    ,sum(minibean)  as minibean
+    ,sum(minipoint) as minipoint
+    from (
+        select uin,map_id,minicoin,0 as minibean,0 as minipoint from minicoin_data
+        union all
+        select uin,map_id,0 as minicoin,minibean,0 as minipoint from minibean_data
+        union all
+        select uin,map_id,0 as minicoin,0 as minibean,minipoint from minipoint_data
+    )
+    group by 1,2
+)
+select t1.uin,t1.map_id,t2.ctype
+,minicoin
+,minibean
+,minipoint
 ,(select dt from args) as dt
-from map_data t1
-left join minicoin_data  t2 on t1.map_id=t2.map_id
-left join minibean_data  t3 on t1.map_id=t3.map_id
-left join minipoint_data t4 on t1.map_id=t4.map_id
-where coalesce(minicoin,minibean,minipoint) is not null --至少一种消费记录
-order by 1 desc
+from pay_data t1
+inner join map_data t2 on t1.map_id=t2.map_id
 ;
